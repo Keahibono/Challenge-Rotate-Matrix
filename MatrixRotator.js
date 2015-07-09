05aee5f7a68acabca88b94e2c539355f309c9945
Paul Selhorst <paulselhorst@Pauls-MacBook-Pro.local>
2015-07-08 16:55:18 -1000

    First Commit

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -13,15 +13,30 @@ var Direction = require("./Direction").Direction;
 
 function MatrixRotator(matrix){
   this.matrix = matrix;
-  
-};
+}
 
+
+var newMatrixArray =[];
+var arr =[];
+var x;
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
 MatrixRotator.prototype.rotate = function(direction) {
   // do work here
+  var MTX = this.matrix;
+  var len = MTX.length;
 
+  for (var i = 0; i < len; i++){
+  	for (var j = 0; j < len; j++){
+  		var shifted = MTX[j].shift()
+  		arr.push(shifted)
+  	}
+  	arr.reverse();
+  	newMatrixArray.push(arr);
+    
+  }
+  //newMatrixArray.splice(len, 5);
+  //this.matrix = newMatrixArray;
+  //console.log(newMatrixArray);
   // must be a valid Direction, see Direction.js
-
-
-};
+}
